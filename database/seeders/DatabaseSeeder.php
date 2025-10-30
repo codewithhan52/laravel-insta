ee01f5c89ded910cde4b02934a961dd74dabc1ae
hanifazzuhdi <hnfhanif52@gmail.com>
2025-10-31 00:37:35 +0700

    Add migration and model files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,25 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Seed the application's database.
+	 */
+	public function run(): void {
 
-        User::firstOrCreate(
-            ['email' => 'test@example.com'],
-            [
-                'name' => 'Test User',
-                'password' => 'password',
-                'email_verified_at' => now(),
-            ]
-        );
-    }
+		User::firstOrCreate(
+			[ 'email' => 'test@example.com' ],
+			[
+				'name'              => 'Test User',
+				'password'          => 'password',
+				'email_verified_at' => now(),
+			]
+		);
+
+		User::factory( 10 )->create();
+	}
 }
