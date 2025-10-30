7eb5f7880d4b0531e6e3c751beef3cd533cb38a6
hanifazzuhdi <hnfhanif52@gmail.com>
2025-10-31 01:33:58 +0700

    Rename like table name to pots_likes

--- database/migrations/2025_10_30_163940_create_likes_table.php
+++ database/migrations/2025_10_30_163940_create_likes_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
 	 * Run the migrations.
 	 */
 	public function up(): void {
-		Schema::create( 'likes', function ( Blueprint $table ) {
+		Schema::create( 'post_likes', function ( Blueprint $table ) {
 			$table->id();
 			$table->foreignId( 'post_id' )->constrained()->onDelete( 'cascade' );
 			$table->foreignId( 'user_id' )->constrained()->onDelete( 'cascade' );
@@ -21,6 +21,6 @@ return new class extends Migration {
 	 * Reverse the migrations.
 	 */
 	public function down(): void {
-		Schema::dropIfExists( 'likes' );
+		Schema::dropIfExists( 'post_likes' );
 	}
 };
